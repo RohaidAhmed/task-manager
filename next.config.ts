94b83cbc34d8dedae59021a5447e8d2419974abe
RohaidAhmed <rohaidahmed123@gmail.com>
2025-09-02 17:00:15 +0500

    fixes

--- next.config.ts
+++ next.config.ts
@@ -1,21 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   eslint: {
     ignoreDuringBuilds: true,
   },
-  // async rewrites() {
-  //   return [
-  //     {
-  //       source: "/api/v1/:path*",
-  //       destination: "/api/v1/:path*"
-  //     },
-  //   ];
-  // },
   env: {
-    // API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL || 'http://localhost:3000',
-    MONGODB_URI: process.env.MONGO_URI,
+    MONGODB_URI: process.env.MONGODB_URI, // Make sure this matches Vercel env var name
   },
   allowedDevOrigins: ['local-origin.dev', '*.local-origin.dev', 'localhost', 'task-manager-rohaid.vercel.app'],
 };
